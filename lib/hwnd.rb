b2c826a5055026a67301dfb861956284264c6a2c
Ethan <ethan@medidata>
2009-06-02 14:58:25 +0000

    hwnd: add some more useful methods (mostly ones wrapping ShowWindow), make errors more specific, note errors in comments.

--- lib/hwnd.rb
+++ lib/hwnd.rb
@@ -1,9 +1,28 @@
 require 'dl/import'
 require 'dl/struct'
 
-class HWNDError < StandardError;end
-
 class HWND
+  # Class that wraps useful methods of user32.dll involving hwnd's 
+  #
+  #--
+  #
+  # todo: 
+  # * maybe use a class or module with include DL::Importable and extern 
+  #   functions used instead of calling User32['whatever', '...'] every time
+  #   (see win32screenshot.rb)
+  # * make sure the signatures being used are correct with the letter codings (bools are chars in some places, pointers, voids may be wrong)
+  # * GetTitleBarInfo http://msdn.microsoft.com/en-us/library/ms633513(VS.85).aspx
+  # * GetWindowInfo http://msdn.microsoft.com/en-us/library/ms633516(VS.85).aspx http://msdn.microsoft.com/en-us/library/ms632610(VS.85).aspx
+  # * ? ShowOwnedPopups http://msdn.microsoft.com/en-us/library/ms633547(VS.85).aspx
+  # * FindWindow http://msdn.microsoft.com/en-us/library/ms633499(VS.85).aspx
+  # * other useful stuff, see http://msdn.microsoft.com/en-us/library/ms632595(VS.85).aspx
+  # * expand SendMessage / PostMessage http://msdn.microsoft.com/en-us/library/ms644950(VS.85).aspx http://msdn.microsoft.com/en-us/library/ms644944(VS.85).aspx
+  
+  class Error < StandardError;end
+  class SystemError < Error;end
+  class NotExistsError < Error;end
+  class MatchError < Error;end
+  
   User32 = DL.dlopen("user32")
   Kernel32 = DL.dlopen("kernel32")
 
@@ -17,6 +36,7 @@ class HWND
   WM_GETTEXT  = 0x000D
   WM_GETTEXTLENGTH = 0xE
 
+  #--
   # GetWindows constants
   GW_HWNDFIRST = 0
   GW_HWNDLAST = 1
@@ -27,25 +47,30 @@ class HWND
   GW_ENABLEDPOPUP = 6
   GW_MAX = 6
 
+  #--
   # GetAncestor constants
   GA_PARENT = 1
   GA_ROOT = 2
   GA_ROOTOWNER = 3 
   
+  #--
   # ShowWindow constants - http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
-  #SW_FORCEMINIMIZE   = ? # Windows 2000/XP: Minimizes a window, even if the thread that owns the window is not responding. This flag should only be used when minimizing windows from a different thread.
   SW_HIDE            = 0 # Hides the window and activates another window.
-  SW_MAXIMIZE       = 11 # Maximizes the specified window.
-  SW_MINIMIZE        = 6 # Minimizes the specified window and activates the next top-level window in the Z order.
-  SW_RESTORE         = 9 # Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window.
-  SW_SHOW            = 5 # Activates the window and displays it in its current size and position. 
-  SW_SHOWDEFAULT    = 10 # Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application. 
-  SW_SHOWMAXIMIZED   = 3 # Activates the window and displays it as a maximized window.
+  SW_SHOWNORMAL      = 1 # Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
   SW_SHOWMINIMIZED   = 2 # Activates the window and displays it as a minimized window.
+  SW_SHOWMAXIMIZED   = 3 # Activates the window and displays it as a maximized window.
+  SW_MAXIMIZE        = 3 # Maximizes the specified window. 
+                         #--
+                         # there seems to be no distinct SW_MAXIMIZE (but there is a distinct SW_MINIMIZE), just the same as SW_SHOWMAXIMIZED
+                         # some references define SW_MAXIMIZE as 11, which seems to just be wrong; that is correctly SW_FORCEMINIMIZE
+  SW_SHOWNOACTIVATE  = 4 # Displays a window in its most recent size and position. This value is similar to SW_SHOWNORMAL, except the window is not actived.
+  SW_SHOW            = 5 # Activates the window and displays it in its current size and position. 
+  SW_MINIMIZE        = 6 # Minimizes the specified window and activates the next top-level window in the Z order.
   SW_SHOWMINNOACTIVE = 7 # Displays the window as a minimized window. This value is similar to SW_SHOWMINIMIZED, except the window is not activated.
   SW_SHOWNA          = 8 # Displays the window in its current size and position. This value is similar to SW_SHOW, except the window is not activated.
-  SW_SHOWNOACTIVATE  = 4 # Displays a window in its most recent size and position. This value is similar to SW_SHOWNORMAL, except the window is not actived.
-  SW_SHOWNORMAL      = 1 # Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
+  SW_RESTORE         = 9 # Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when restoring a minimized window.
+  SW_SHOWDEFAULT    = 10 # Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application. 
+  SW_FORCEMINIMIZE  = 11 # Windows 2000/XP: Minimizes a window, even if the thread that owns the window is not responding. This flag should only be used when minimizing windows from a different thread.
 
   WIN_TRUE=-1
   WIN_FALSE=0
@@ -53,6 +78,8 @@ class HWND
   attr_reader :hwnd
 
   # takes a hwnd handle (integer) 
+  #
+  # raises ArgumentError if the hwnd is not a Fixnum greater than 0
   def initialize(hwnd)
     raise ArgumentError, "hwnd must be an integer greater than 0" unless hwnd.is_a?(Fixnum) && hwnd > 0
     @hwnd=hwnd
@@ -240,6 +267,15 @@ class HWND
     ret != WIN_FALSE
   end
 
+  # whether the window is minimized (iconic).
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633527(VS.85).aspx
+  def iconic?
+    ret, args=User32['IsIconic', 'CL'].call(hwnd)
+    ret != WIN_FALSE
+  end
+  alias minimized? iconic?
+  
   # switch focus and bring to the foreground
   # the argument alt_tab, if true, indicates that the window is being switched to using the Alt/Ctl+Tab key sequence. This argument should be false otherwise.
   #
@@ -248,12 +284,135 @@ class HWND
     void, args=User32['SwitchToThisWindow','pLC'].call(hwnd, alt_tab ? WIN_TRUE : WIN_FALSE)
     nil
   end
+
+  # puts the thread that created the specified window into the foreground and activates the window. Keyboard input is directed to the window, and various visual cues are changed for the user. The system assigns a slightly higher priority to the thread that created the foreground window than it does to other threads.
+  # If the window was brought to the foreground, the return value is true.
+  # If the window was not brought to the foreground, the return value is false.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633539(VS.85).aspx
+  def set_foreground!
+    ret, args=User32['SetForegroundWindow','CL'].call(hwnd)
+    ret != WIN_FALSE
+  end
   
-  # minimizes this window
+  # brings the window to the top of the Z order. If the window is a top-level window, it is activated. If the window is a child window, the top-level parent window associated with the child window is activated.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms632673(VS.85).aspx
+  def bring_to_top!
+    ret, args=User32['BringWindowToTop','CL'].call(hwnd)
+    ret != WIN_FALSE
+  end
+  
+  # minimizes this window (but does not destroy it)
+  # (why is it called close? I don't know)
   #
   # http://msdn.microsoft.com/en-us/library/ms632678(VS.85).aspx
+  def close!
+    ret, args= User32['CloseWindow', 'CL'].call(hwnd)
+    ret != WIN_FALSE
+  end
+
+  # Hides the window and activates another window.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def hide!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_HIDE)
+    ret != WIN_FALSE
+  end
+
+  # Activates and displays a window. If the window is minimized or maximized, the system restores it to its original size and position. An application should specify this flag when displaying the window for the first time.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def show_normal!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_SHOWNORMAL)
+    ret != WIN_FALSE
+  end
+
+  # Activates the window and displays it as a minimized window.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def show_minimized!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_SHOWMINIMIZED)
+    ret != WIN_FALSE
+  end
+
+  # Activates the window and displays it as a maximized window. (note: exact same as maximize!)
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def show_maximized!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_SHOWMAXIMIZED)
+    ret != WIN_FALSE
+  end
+
+  # Maximizes this window. (note: exact same as show_maximized!)
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def maximize!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_MAXIMIZE)
+    ret != WIN_FALSE
+  end
+
+  # Displays the window in its most recent size and position. This is similar to show_normal!, except the window is not actived.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def show_no_activate!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_SHOWNOACTIVATE)
+    ret != WIN_FALSE
+  end
+
+  # Activates the window and displays it in its current size and position. 
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def show!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_SHOW)
+    ret != WIN_FALSE
+  end
+
+  # Minimizes this window and activates the next top-level window in the Z order.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
   def minimize!
-    ret, args= User32['CloseWindow', 'CI'].call(hwnd)
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_MINIMIZE)
+    ret != WIN_FALSE
+  end
+
+  # Displays the window as a minimized window. This is similar to show_minimized!, except the window is not activated.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def show_min_no_active!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_SHOWMINNOACTIVE)
+    ret != WIN_FALSE
+  end
+
+  # Displays the window in its current size and position. This is similar to show!, except the window is not activated.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def show_na!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_SHOWNA)
+    ret != WIN_FALSE
+  end
+
+  # Activates and displays the window. If the window is minimized or maximized, the system restores it to its original size and position. An application should use this when restoring a minimized window.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def restore!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_RESTORE)
+    ret != WIN_FALSE
+  end
+
+  # Sets the show state based on the SW_ value specified in the STARTUPINFO structure passed to the CreateProcess function by the program that started the application. 
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def show_default!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_SHOWDEFAULT)
+    ret != WIN_FALSE
+  end
+
+  # Windows 2000/XP: Minimizes the window, even if the thread that owns the window is not responding. This should only be used when minimizing windows from a different thread.
+  #
+  # http://msdn.microsoft.com/en-us/library/ms633548(VS.85).aspx
+  def force_minimize!
+    ret, args= User32['ShowWindow', 'CLI'].call(hwnd, SW_FORCEMINIMIZE)
     ret != WIN_FALSE
   end
 
@@ -285,14 +444,15 @@ class HWND
     nil
   end
 
-  # iterates over each child, yielding a HWND object 
+  # iterates over each child, yielding a HWND object. 
+  # raises a HWND::NotExistsError if the window does not exist, or a HWND::SystemError if a System Error errors.
   # use #children to get an Enumerable object. 
   #
   # http://msdn.microsoft.com/en-us/library/ms633494(VS.85).aspx
   #
   # For System Error Codes see http://msdn.microsoft.com/en-us/library/ms681381(VS.85).aspx
   def each_child
-    raise HWNDError.new, "Window does not exist! Cannot enumerate children." unless exists?
+    raise HWND::NotExistsError.new, "Window does not exist! Cannot enumerate children." unless exists?
     enum_child_windows_callback= DL.callback('ILL') do |chwnd, lparam|
       yield HWND.new(chwnd)
       WIN_TRUE
@@ -304,7 +464,7 @@ class HWND
     end
     if ret==0
       code, args=Kernel32['GetLastError','I'].call
-      raise HWNDError, "EnumChildWindows encountered an error (System Error Code #{code})"
+      raise HWND::SystemError, "EnumChildWindows encountered an error (System Error Code #{code})"
       # actually, EnumChildWindows doesn't say anything about return value indicating error encountered.
       # Although EnumWindows does, so it seems sort of safe to assume that would apply here too. 
       # but, maybe not - so, should we raise an error here? 
@@ -314,16 +474,21 @@ class HWND
   
   # returns an Enumerable object that can iterate over each child of this HWND, 
   # yielding a HWND object 
+  #
+  # may raise a HWND::SystemError from #each_child 
   def children
     Children.new self
   end
 
-  def ==(oth)
-    self.eql?(oth)
-  end
+  # true if comparing an object of the same class with the same hwnd (integer) 
   def eql?(oth)
     oth.class==self.class && oth.hwnd==self.hwnd
   end
+  alias == eql?
+#  def ==(oth)
+#    self.eql?(oth)
+#  end
+
   def hash
     [self.class, self.hwnd].hash
   end
@@ -335,21 +500,32 @@ class HWND
   # Give the name of a button, or a Regexp to match it (see #child_button).
   # keeps clicking the button until the button no longer exists, or until 
   # the given block is true (ie, not false or nil)
+  # Options:
+  # * :interval is the length of time in seconds between each attempt (default 0.05)
+  # * :switch_to is whether #switch_to! should be called on self before each attempt, since button-clicking is much more likely to fail if the window isn't focused (default true)
+  # * :exception is the exception class or instance that will be raised if we can't click the button (default nil, no exception is raised, the return value indicates success/failure)
+  #
+  # Raises ArgumentError if invalid options are given. 
+  # Raises a HWND::NotExistsError if the button doesn't exist, or if this window doesn't exist, or a HWND::SystemError if a System Error occurs (from #each_child)
   def click_child_button_try_for!(button_text, time, options={})
-    handle_options!({:interval => 0.5, :switch_to => true, :exception => nil}, options)
-    button=child_button(button_text) || (raise HWNDError, "Button #{button_text.inspect} not found")
     require 'spigot/utilities' #lazy load
-    condition=proc{!button.exists? || (block_given? && yield)}
-    Waiter.try_for(time, :interval => options[:interval], :condition => condition, :exception => options[:exception]) do
+    handle_options!(options, {:switch_to => true, :exception => nil, :interval => 0.05})
+    button=child_button(button_text) || (raise HWND::NotExistsError, "Button #{button_text.inspect} not found")
+    waiter_options={}
+    waiter_options[:condition]=proc{!button.exists? || (block_given? && yield)}
+    waiter_options.merge!(options.reject{|k,v| ![:exception, :interval].include?(k)})
+    Waiter.try_for(time, waiter_options) do
       switch_to! if options[:switch_to]
       button.click!
     end
-    return condition.call
+    return waiter_options[:condition].call
   end
 
   # returns a HWND that is a child of this that matches the given button_text (Regexp or #to_s-able) 
   # or nil if no such child exists. 
   # & is stripped when matching so don't include it. String comparison is case-insensitive. 
+  #
+  # May raise a HWND::SystemError from #each_child
   def child_button(button_text)
     children.detect do |child|
       child.class_name=='Button' && button_text.is_a?(Regexp) ? child.text.tr('&', '') =~ button_text : child.text.tr('&', '').downcase==button_text.to_s.tr('&', '').downcase
@@ -358,6 +534,8 @@ class HWND
   
   # Iterates over every hwnd yielding an HWND object. 
   # use HWND::All if you want an Enumerable object. 
+  #
+  # Raises a HWND::SystemError if a System Error occurs. 
   # 
   # http://msdn.microsoft.com/en-us/library/ms633497.aspx
   #
@@ -374,12 +552,14 @@ class HWND
     end
     if ret==0
       code, args=Kernel32['GetLastError','I'].call
-      raise HWNDError, "EnumWindows ecountered an error (System Error Code #{code})"
+      raise HWND::SystemError, "EnumWindows ecountered an error (System Error Code #{code})"
     end
     nil
   end
   
   # returns the first HWND found whose text matches what is given
+  #
+  # May raise a HWND::SystemError from HWND.each_hwnd
   def self.find_first_by_text(text)
     HWND::All.detect do |hwnd|
       text===hwnd.text # use triple-equals so regexps try to match, strings see if equal 
@@ -387,26 +567,32 @@ class HWND
   end
 
   # returns all HWND objects found whose text matches what is given
+  #
+  # May raise a HWND::SystemError from HWND.each_hwnd
   def self.find_all_by_text(text)
     HWND::All.select do |hwnd|
       text===hwnd.text # use triple-equals so regexps try to match, strings see if equal 
     end
   end
 
-  # raises an error if more than one window matching given text is found
+  # raises a HWND::MatchError if more than one window matching given text is found, 
   # so that you can be sure you are attaching to the right one (because it's the only one)
-  def self.find_only_by_text(text)
+  #
+  # May also raise a HWND::SystemError from HWND.each_hwnd
+ def self.find_only_by_text(text)
     matched=HWND::All.select do |hwnd|
       text===hwnd.text
     end
     if matched.size != 1
-      raise HWNDError, "Found #{matched.size} windows matching #{text.inspect}; there should be one"
+      raise MatchError, "Found #{matched.size} windows matching #{text.inspect}; there should be one"
     else
       return matched.first
     end
   end
 
   # Enumerable object that iterates over every available HWND 
+  #
+  # May raise a HWND::SystemError from HWND.each_hwnd
   module All
     def self.each
       HWND.each_hwnd do |hwnd|
@@ -417,6 +603,8 @@ class HWND
   end
 
   # instantiates Enumerable objects that iterate over a HWND's children. 
+  #
+  # May raise a HWND::SystemError from HWND#each_child
   class Children
     attr_reader :parenthwnd
     def initialize(parenthwnd)
